201df6e7e691320be0a06782fc24ec7e0014d332
Edward P. Legaspi <czetsuya@gmail.com>
2021-03-21 14:34:06 +0800

    feat: add replace utility function

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,11 +1,24 @@
 export const replaceFirstN = (str, n) => {
+
   const replace = `^.{1,${n}}`
   const re = new RegExp(replace)
   return str.replace(re, m => "X".repeat(m.length))
 }
 
 export const replaceLastN = (str, n) => {
+
   const replace = `(.{1,${n}})$`
   const re = new RegExp(replace)
+  return str.replace(re, m => Array(m.length + 1).join("X"))
+}
+
+export const replaceFirstMAndLastN = (str, m, n) => {
+
+  let replace = `^.{1,${m}}`
+  let re = new RegExp(replace)
+  str = str.replace(re, m => "X".repeat(m.length))
+
+  replace = `(.{1,${n}})$`
+  re = new RegExp(replace)
   return str.replace(re, m => Array(m.length + 1).join("X"))
 }
